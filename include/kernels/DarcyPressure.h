cdbcd60464b57f40ee98e7f776710328fc243cea
andy <laumiulunn@gmail.com>
2018-12-13 14:12:01 -0700

    added DarcyPresssure

---
+++ include/kernels/DarcyPressure.h
@@ -0,0 +1,35 @@
+
+#ifndef DARCYPRESSURE_H
+#define DARCYPRESSURE_H
+// Including the "Diffusion" Kernel here so we can extend it
+
+#include "Diffusion.h"
+
+class DarcyPressure;
+template <>
+InputParameters validParams<DarcyPressure>();
+/**
+ * Computes the residual contribution: K / mu * grad_u * grad_phi.
+ *
+ * We are inheriting from Diffusion instead of from Kernel because
+ * the grad_u * grad_phi is already coded in there and all we
+ * need to do is specialize that calculation by multiplying by K / mu.
+ */
+class DarcyPressure : public Diffusion
+{
+public:
+  DarcyPressure(const InputParameters & parameters);
+protected:
+  /**
+   * Kernels _must_ override computeQpResidual()
+   */
+  virtual Real computeQpResidual() override;
+  /**
+   * This is optional (but recommended!)
+   */
+  virtual Real computeQpJacobian() override;
+  /// Will be set from the input file
+  Real _permeability;
+  Real _viscosity;
+};
+#endif // DARCYPRESSURE_H
